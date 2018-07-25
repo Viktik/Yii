70608ef9879a35b89a9120f6007d9966c33d6b27
Viktik <viktor.askor@gmail.com>
2018-07-25 10:00:11 +0300

    migration posts

---
+++ console/migrations/m180725_065824_posts.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180725_065824_posts
+ */
+class m180725_065824_posts extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180725_065824_posts cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180725_065824_posts cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
