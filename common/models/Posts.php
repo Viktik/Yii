695b9600439e4f97f041b6b4ed494c66451021c6
Viktik <viktor.askor@gmail.com>
2018-07-25 11:18:37 +0300

    Active Record

---
+++ common/models/Posts.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "posts".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $text
+ * @property int $user_id
+ * @property string $alias
+ */
+class Posts extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'posts';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id'], 'integer'],
+            [['title', 'alias'], 'string', 'max' => 200],
+            [['text'], 'string', 'max' => 2000],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'text' => 'Text',
+            'user_id' => 'User ID',
+            'alias' => 'Alias',
+        ];
+    }
+}
