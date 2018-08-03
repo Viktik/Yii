df28ce6928e8276fe01f503b8c4a865a1c676f73
Viktik <viktor.askor@gmail.com>
2018-08-03 13:32:11 +0300

    Singleton

--- backend/controllers/SingletonController.php
+++ backend/controllers/SingletonController.php
@@ -2,12 +2,39 @@
 
 namespace backend\controllers;
 
+use common\models\Posts;
+use common\models\User;
+use common\models\Test;
+
 class SingletonController extends \yii\web\Controller
 {
     public function actionIndex($className = '')
     {
+        switch ($className) {
+            case 'Posts':
+                $class = new Posts();
+                break;
+            case 'User':
+                $class = new User();
+                break;
+            case 'Test':
+                $class = new Test();
+                break;
+            default: $class = false;
+        }
+
+        if ($class instanceof \Singleton) {
+            $result = 'Single';
+        } else {
+            $result = 'nope';
+        }
+
+        if (!$class){
+            $result = 'class does not exist';
+        }
+
         return $this->render('index', [
-            'className' => $className,
+            'result' => $result,
         ]);
     }
 
--- backend/views/singleton/index.php
+++ backend/views/singleton/index.php
@@ -1,9 +1,4 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>singleton/index</h1>
-<?= $className ?>
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<h1><?= $result ?></h1>
