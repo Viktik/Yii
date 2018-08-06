c6a2722734e9e7a5e9e0f984f40d44a582c4ca1c
Viktik <viktor.askor@gmail.com>
2018-08-06 13:33:29 +0300

    UserPage

--- backend/views/site/userPage.php
+++ backend/views/site/userPage.php
@@ -1,33 +1,35 @@
 <?php
 ?>
-<table class="table table-user-information">
-    <tbody>
-    <tr>
-        <td>Hire date:</td>
-        <td>06/23/2013</td>
-    </tr>
-    <tr>
-        <td>Date of Birth</td>
-        <td>01/24/1988</td>
-    </tr>
-    
-    <tr>
-        <td>Gender</td>
-        <td>Female</td>
-    </tr>
-    <tr>
-        <td>Home Address</td>
-        <td>Kathmandu,Nepal</td>
-    </tr>
-    <tr>
-        <td>Email</td>
-        <td><a href="mailto:info@support.com">info@support.com</a></td>
-    </tr>
-    <td>Phone Number</td>
-    <td>123-4567-890(Landline)<br><br>555-4567-890(Mobile)
-    </td>
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?=$user->username?></h3>
+    </div>
+    <div class="panel-body">
+        <div class="row">
+            <div class="col-md-3 col-lg-3 " align="center">
+                <img alt="User Pic"
+                     src="http://babyinfoforyou.com/wp-content/uploads/2014/10/avatar-300x300.png"
+                     class="img-circle img-responsive"></div>
 
-    </tr>
+            <div class=" col-md-9 col-lg-9 ">
+                <table class="table table-user-information">
+                    <tbody>
+                    <tr>
+                        <td>Name:</td>
+                        <td><?= $user->username ?></td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><?= $user->email ?></td>
+                    </tr>
 
-    </tbody>
-</table>
+                    <tr>
+                        <td>Creation date</td>
+                        <td><?= date('H:i D-m-Y', $user->created_at) ?></td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
